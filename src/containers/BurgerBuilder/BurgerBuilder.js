5701c54738243ada47c724b2f044fad5b93624a5
Rudha Oliveira <rudha94@gmail.com>
2018-03-22 17:22:33 -0300

    Implemented first routes.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,28 +93,29 @@ class BurgerBuilder extends Component {
 
 	purchaseContinueHandler = () => {
 		//alert('You continue!');
-		this.setState({loading: true});
-		const order = {
-			ingredients: this.state.ingredients,
-			price: this.state.totalPrice,
-			customer: {
-				name: 'Rudha Oliveira',
-				address: {
-					street: 'Test',
-					zipCode: '54365',
-					country: 'Brazil',
-				},
-				email: 'test@test.com',
-			},
-			deliveryMethod: 'fastest',
-		}
-		axios.post('/orders.json', order)
-			.then(response => {
-				this.setState({loading: false, purchasing: false})
-			})
-			.catch(error => {
-				this.setState({loading: false, purchasing: false})
-			});
+		// this.setState({loading: true});
+		// const order = {
+		// 	ingredients: this.state.ingredients,
+		// 	price: this.state.totalPrice,
+		// 	customer: {
+		// 		name: 'Rudha Oliveira',
+		// 		address: {
+		// 			street: 'Test',
+		// 			zipCode: '54365',
+		// 			country: 'Brazil',
+		// 		},
+		// 		email: 'test@test.com',
+		// 	},
+		// 	deliveryMethod: 'fastest',
+		// }
+		// axios.post('/orders.json', order)
+		// 	.then(response => {
+		// 		this.setState({loading: false, purchasing: false})
+		// 	})
+		// 	.catch(error => {
+		// 		this.setState({loading: false, purchasing: false})
+		// 	});
+		this.props.history.push('/checkout');
 	}
 
 	render () {
