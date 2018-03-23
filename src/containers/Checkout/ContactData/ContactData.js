1eb23e40e14be8647115aff0babd6f264716ce0f
Rudha Oliveira <rudha94@gmail.com>
2018-03-23 16:27:22 -0300

    Added basic forms.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,14 +3,61 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
 	state = {
-		name: '',
-		email: '',
-		address: {
-			street: '',
-			zipCode: '',
+		orderForm: {
+			name: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Your Name',
+				},
+				value: '',
+			},
+			street: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Street',
+				},
+				value: '',
+			},
+			zipCode: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'ZIP Code',
+				},
+				value: '',
+			},
+			country: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'text',
+					placeholder: 'Country',
+				},
+				value: '',
+			},
+			email: {
+				elementType: 'input',
+				elementConfig: {
+					type: 'email',
+					placeholder: 'Your Email',
+				},
+				value: '',
+			},
+			deliveryMethod: {
+				elementType: 'select',
+				elementConfig: {
+					options: [
+						{value: 'fastest', displayValue: 'Fastest'},
+						{value: 'cheapest', displayValue: 'Cheapest'},
+					],
+				},
+				value: '',
+			},
 		},
 		loading: false,
 	}
@@ -18,19 +65,14 @@ class ContactData extends Component {
 	orderHandler = (event) => {
 		event.preventDefault();
 		this.setState({loading: true});
+		const formData = {};
+		for (let formElementIdentifier in this.state.orderForm) {
+			formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+		}
 		const order = {
 			ingredients: this.props.ingredients,
 			price: this.props.price,
-			customer: {
-				name: 'Rudha Oliveira',
-				address: {
-					street: 'Test',
-					zipCode: '54365',
-					country: 'Brazil',
-				},
-				email: 'test@test.com',
-			},
-			deliveryMethod: 'fastest',
+			orderData: formData,
 		}
 		axios.post('/orders.json', order)
 			.then(response => {
@@ -42,14 +84,34 @@ class ContactData extends Component {
 			});
 	}
 
+	inputChangedHandler = (event, inputIdentifier) => {
+		const updatedOrderForm = {...this.state.orderForm};
+		const updatedFormElement = {...updatedOrderForm[inputIdentifier]};
+		updatedFormElement.value = event.target.value;
+		updatedOrderForm[inputIdentifier] = updatedFormElement;
+		this.setState({orderForm: updatedOrderForm});
+	}
+
 	render () {
+		const formElementsArray = [];
+		for (let key in this.state.orderForm) {
+			formElementsArray.push({
+				id: key,
+				config: this.state.orderForm[key],
+			});
+		}
 		let form = (
-			<form>
-					<input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-					<input className={classes.Input} type="email" name="email" placeholder="Your Email" />
-					<input className={classes.Input} type="text" name="street" placeholder="Street Name" />
-					<input className={classes.Input} type="text" name="zip" placeholder="Postal Code" />
-					<Button buttonType="Success" clicked={this.orderHandler} >Order</Button>
+			<form onSubmit={this.orderHandler} >
+				{formElementsArray.map(formElement => (
+					<Input
+						key={formElement.id}
+						elementType={formElement.config.elementType}
+						elementConfig={formElement.config.elementConfig}
+						value={formElement.config.value}
+						changed={(event) => this.inputChangedHandler(event, formElement.id)}
+					/>
+				))}
+				<Button buttonType="Success">Order</Button>
 			</form>
 		);
 		if (this.state.loading) {
