7da9f51023f9e5a5240470289ac13620c100b576
Rudhá Oliveira <rudha94@gmail.com>
2018-03-27 15:09:35 -0300

    Async Redux.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,30 +4,22 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
-import * as actionTypes from '../../store/actions';
+import * as actionTypes from '../../store/actions/index';
+import axios from '../../axios-orders';
 
 class BurgerBuilder extends Component {
 	// constructor(props) {
 	// 	super(props);
 	// 	this.state = {...}
 	// }
-	state ={
+	state = {
 		purchasing: false,
-		loading: false,
-		error: false,
 	}
 
 	componentDidMount () {
-		// axios.get('/ingredients.json')
-		// 	.then(response => {
-		// 		this.setState({ingredients: response.data});
-		// 	})
-		// 	.catch(error => {
-		// 		this.setState({error: true});
-		// 	});
+		this.props.onInitIngredients();
 	}
 
 	updatePurchaseState (ingredients) {
@@ -96,7 +88,7 @@ class BurgerBuilder extends Component {
 		}
 
 		let orderSummary = null;
-		let burger = this.state.error ? <p>Ingredients can't be loaded!</p> : <Spinner />;
+		let burger = this.props.error ? <p>Ingredients can't be loaded!</p> : <Spinner />;
 		if (this.props.ings) {
 			burger = (
 				<Fragment> 
@@ -118,9 +110,6 @@ class BurgerBuilder extends Component {
 				price={this.props.price}
 			/>
 		}
-		if (this.state.loading) {
-			orderSummary = <Spinner />
-		}
 
 		return (
 			<Fragment>
@@ -137,13 +126,15 @@ const mapStateToProps = state => {
 	return {
 		ings: state.ingredients,
 		price: state.totalPrice,
+		error: state.error,
 	};
 }
 
 const mapDispatchToProps = dispatch => {
 	return {
-		onIngredientAdded: (ingName) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: ingName}),
-		onIngredientRemoved: (ingName) => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: ingName}),
+		onIngredientAdded: (ingName) => dispatch(actionTypes.addIngredient(ingName)),
+		onIngredientRemoved: (ingName) => dispatch(actionTypes.removeIngredient(ingName)),
+		onInitIngredients: () => dispatch(actionTypes.initIngredients()),
 	};
 }
 
