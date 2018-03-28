238fcb0a3aa9fdb9bf4244d6addcaea68391fb5e
Rudhá Oliveira <rudha94@gmail.com>
2018-03-28 17:25:55 -0300

    Added token based auth.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.css';
 import * as actions from '../../store/actions/index';
 
@@ -95,7 +96,7 @@ class Auth extends Component {
 			});
 		}
 
-		const form = formElementsArray.map(formElement => (
+		let form = formElementsArray.map(formElement => (
 			<Input
 				key={formElement.id}
 				elementType={formElement.config.elementType}
@@ -108,8 +109,21 @@ class Auth extends Component {
 			/>
 		));
 
+		if (this.props.loading) {
+			form = <Spinner />;
+		}
+
+		let errorMessage = null;
+
+		if (this.props.error) {
+			errorMessage = (
+				<p>{this.props.error.message}</p>
+			);
+		}
+
 		return (
 			<div className={classes.Auth}>
+				{errorMessage}
 				<form onSubmit={this.submitHandler} >
 					{form}	
 					<Button buttonType="Success">Submit</Button>
@@ -123,10 +137,17 @@ class Auth extends Component {
 	}
 }
 
+const mapStateToProps = state => {
+	return {
+		loading: state.auth.loading,
+		error: state.auth.error,
+	};
+};
+
 const mapDispatchToProps = dispatch => {
 	return {
 		onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
 	};
 };
 
-export default connect(null, mapDispatchToProps)(Auth);
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
