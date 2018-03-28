9e3f1b2114321d8bfc93e0f27e6a626084d517d9
Rudhá Oliveira <rudha94@gmail.com>
2018-03-28 15:40:55 -0300

    Added authentication basics.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 
 class App extends Component {
   render() {
@@ -12,7 +13,8 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" exact component={Orders} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/auth" component={Auth} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -48,6 +48,7 @@ class ContactData extends Component {
 					required: true,
 					minLength: 5,
 					maxLength: 5,
+					isNumeric: true,
 				},
 				valid: false,
 				touched: false,
@@ -74,6 +75,7 @@ class ContactData extends Component {
 				value: '',
 				validation: {
 					required: true,
+					isEmail: true,
 				},
 				valid: false,
 				touched: false,
@@ -119,6 +121,14 @@ class ContactData extends Component {
 		if (rules.maxLength) {
 			isValid = value.length <= rules.maxLength && isValid;
 		}
+		if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
 		return isValid;
 	}
 
