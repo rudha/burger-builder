a8c6df5372684e0c5c074ac6abc0de816b7430cb
Rudhá Oliveira <rudha94@gmail.com>
2018-04-04 19:51:55 -0300

    Added improvements from module 19.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -51,7 +51,6 @@ export const auth = (email, password, isSignup) => {
 		if (!isSignup) {url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyC1M7G-UkYyrsrIB5Yf8f8aYrVvde--2Tk';}
 		axios.post(url, authData)
 			.then(response => {
-				console.log(response);
 				const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
 				localStorage.setItem('token', response.data.idToken);
 				localStorage.setItem('expirationDate', expirationDate);
@@ -60,7 +59,6 @@ export const auth = (email, password, isSignup) => {
 				dispatch(checkAuthTimeout(response.data.expiresIn));
 			})
 			.catch(error => {
-				console.log(error);
 				dispatch(authFail(error.response.data.error));
 			});
 	};
